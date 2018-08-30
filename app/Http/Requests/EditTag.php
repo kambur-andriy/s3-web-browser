b4c54b5d468095bee9252c3e70fccf581a8987e7
Pavel Boiko <pavel.boiko.work@gmail.com>
2018-08-30 14:26:49 +0300

    Edit tag request object.

---
+++ app/Http/Requests/EditTag.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+
+
+class EditTag extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+	        'id' => 'required|integer|bail',
+	        'name' => 'required|string|bail',
+	        'category' => 'required|integer|min:1|bail',
+	        'parent_tag' => 'required|integer|bail',
+        ];
+    }
+}
